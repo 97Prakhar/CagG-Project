4a9b91cee68d202c166d86649db6fac624481b1a
prakhasr <prakhar.b.srivastava@capgemini.com>
2020-04-29 08:29:15 +0530

    Minor Changes

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const validator = require("express-validator");
 
 const config = require('./config/config');
 
-mongoose.connect(config.DBPath, (err) => {
-    if (!err) {
-        console.log("Database Connection Succesful");
-    } else {
-        console.log("Error connecting to database")
-    }
-});
+mongoose.connect(config.DBPath, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.error(error));
 
 mongoose.connection.on('connected', () => {
     console.log("Connected to Database : " + config.DBPath);
@@ -28,7 +26,13 @@ const UserRouter = require('./router/router');
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -44,4 +48,6 @@ app.use('/users', UserRouter);
 
 const port = config.Port || 3000;
 
-app.listen(port, () => console.log("Server started at port : "+port));
+app.listen(port, () => console.log("Server started at port : " + port));
+
+app.use(express.static("../client"))
